308e2d68cb0a14be0d1c84984fa371ca4459e104
shernix <eduardo.santiago8@gmail.com>
2020-05-16 01:46:33 -0400

    Added functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,82 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Reverse("AAAACCCGGT"));
+        System.out.println(substringPositions("ATTTTCTAAATATCTAAATTTCTAAATTGATCTAAATTCACCACTGCTTCCTCTAAATTCTAAATTGTCTAAATGTTATCTAAATTCATGTCTAAATATAATCTAAATGAAGTGTCTAAATTTTAATGATCTAAATATATCTAAATGACCTTCTAAATATAGTCTAAATCCTCTAAATTCTAAATGGGTCTAAATCGTCTAAATTCTAAATTCTAAATCTCTAAATTGGTCTCTAAATTATGAGTCTAAATATTTTAAGCGCAGGCTCTAAATGGTACTTACTGGCTGTCTAAATTTTCTAAATTAAATCTAAATGGAAGCGTCTAAATTCTAAATTTCTAAATTCTCTAAATTTCTAAATGGAATCTAAATGCTATCTAAATCAGCAGTATTCTAAATGTTCTAAATAAGAAGTCTAAATATCTAAATCTTCTAAATCACGATCTAAATATCTAAATTCTAAATTATTCGTCTAAATCTCTAAATTCTAAATTCTAAATAAAGATCTAAATCTCATCTAAATACTCTAAATCGGTTTAGTCTAAATGTCTAAATATCTAAATTGTGATCTAAATACTATCTAAATTTAACGACTCTACTCTAAATTCTAAATTCTAAATTGTCTAAATTCTAAATGGGACACAATCTAAATCGTCTAAATTTCTAAATTGAATCTAAATATTACTCTAAATGCGTGTCTAAATATCTAAATTACTCTAAATTCTAAATATCTAAATTGTCTAAATCTCTAAATCACTCTAAATTTCTAAATCTCTCTAAATATTCTAAATCCTGGCGTGTACTCTAAATATGCTCTAAATTGTCTAAATACTCTAAATAATCTAAATGATCTAAATTTCTAAATCTCTAAATCGATCTAAATTCTAAATTTCTAAATTCTAAATTCTAAATAATAATGTCTCTAAATTTGGTGCATCCGTTCTAAATTGG", "TCTAAATTC"));
     }
 
-    public static String Reverse(String t){
+    public static ArrayList<Integer> substringPositions(String DNA, String sub){
+        ArrayList<Integer> indexes = new ArrayList<>();
+
+        for(int i=0; i<DNA.length()-sub.length(); i++){
+            boolean term = true;
+
+            if(DNA.charAt(i) == sub.charAt(0)){
+                for(int j=1; j<sub.length(); j++){
+                    if(DNA.charAt(i + j) != sub.charAt(j)) {
+                        term = false;
+                    }
+                }
+            }
+            if(DNA.charAt(i) == sub.charAt(0) && term == true){
+                indexes.add(i+1);
+            }
+        }
+
+        return indexes;
+    }
+
+    public static int HammingDistance(String s, String t){
+        int dist = 0;
+
+        if(s.length() != t.length())
+            return -1; //return -1 if DNAs have differente lengths
+        for (int i=0; i<s.length(); i++){
+            if(s.charAt(i) != t.charAt(i)){
+                dist++;
+            }
+        }
+
+        return dist;
+    }
+
+    public static char[] Reverse(String t){
         char[] tArr = t.toCharArray();
         int low = 0;
         int high = tArr.length;
         while(low < high){
             char temp = tArr[low];
-            tArr[low] = tArr[high];
-            tArr[high] = tArr[temp];
-            low ++;
+            tArr[low] = tArr[high-1];
+            tArr[high-1] = temp;
+            low++;
             high--;
         }
-        return t;
+        return tArr;
+    }
+
+    public static String ReverseCompliment(String t){
+        char[] reversed = Reverse(t);
+        for(int i=0; i<reversed.length; i++){
+            if(reversed[i] == 'A'){
+                reversed[i] = 'T';
+            }
+            else if(reversed[i] == 'T'){
+                reversed[i] = 'A';
+            }
+            else if(reversed[i] == 'C'){
+                reversed[i] = 'G';
+            }
+            else if(reversed[i] == 'G'){
+                reversed[i] = 'C';
+            }
+        }
+        return new String(reversed);
     }
 
     public static String RNA(String t) {
-        //String used = "TCTGGTTTAGCCCGCACGACGACACCGTATGCCGACGCGGCGTTGCTCCTGACAGTTTATTCTTCAACCCGGTTGGTTCAGGAATTGATTGAGTACGAGAGCGTGGTGTTATAGCCTCTGTAGTCACCAGTCGAGATCCTGTGGTTTTTTGTAGAATCCCCAAAGCGTGGCCGTAGAAGGCACCCGTGCGTTCGGCCTGCGACCGGGTACTAGGCCTGACAGGACTACCTGTCAACAATGGCCCGACTCGCGAACACTAGGTGCTGCAAAGCGTCTATTGGATGGATAGCCCCACTACAGGTTGGGCCCTAGTGGAGGAGTACGATATTGCACGTTATGATAGCGACACTACGTATCAACTTCCCGTAAAGTGTCATCACACGCGTACGGTTAGACTCTCCTGATGTGATTTGAGTAGACTGCTTGCGGACGAGTGCGGGGTAAATGGCTTCTATTATGTTTCGCCTTACACGATGGCTTAGGATTAGACCGTATTCGATACCTGATAGCTCACTATCCCTATTTAAATCATTCAGCGGGTAAATATCGCGACATTATCGCAAGGCCCGTTTCCCCTCCGCCGGCCTAAAAGCCGTGCGGCATGTGGCGGTGTAGACTTCATTAACCACAAAGCCCAAACTCGAACACTGATAGGGGGACCTGGGAGACAGAGGCCCACCAAGTCATATCCAAGACGAACTTGTTTCGACCGGTTCGCACAACCTTGGAATCTGTGCCATTTTGTGAAATTTTTGACGTCATCAGGAGGCGTTCGCTTAGCGCCCCGCTACCCACCCCGCTAGGCTGCTGGGTCCTGGGCTAAGGAGGTAGTAAGCACTCCATTATAGTCAGTATACCAAAACTGACGATGATATCGCCGTGGACGACGAAAATGGGGGTGGCCAGTCAGTTGGTTGTTTGCGCTGCCTGGTGACCAATCAGGATCGATCAGGAGACTTT\n");
         String newt = t.replace('T', 'U');
         return newt;
     }
